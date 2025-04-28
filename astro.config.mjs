63ead57da8e00af2cae7912c6b9989439fdf9860
Nin3 <2091392083@qq.com>
2025-04-28 17:51:27 +0800

    chore: Add `expressive-code` to highlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
+import astroExpressiveCode from 'astro-expressive-code'
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +10,11 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [icon(), mdx()],
+  integrations: [
+    icon(),
+    astroExpressiveCode(),
+    mdx()
+  ],
 
   markdown: {
     shikiConfig: {
